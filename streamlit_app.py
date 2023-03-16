85a1a943c07d60f282577e5dd2609b8ae9527c0d
moralescnoel <64564192+moralescnoel@users.noreply.github.com>
2023-03-16 15:10:33 -0700

    Update streamlit_app.py
    Added testing connector as comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,3 +30,12 @@ caption= product_caption
 streamlit.write('Price: ', df2[1])
 streamlit.write('Sizes Available: ',df2[2])
 streamlit.write(df2[3])
+
+
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),
+#CURRENT_REGION()")
+#my_data_row = my_cur.fetchone()
+#streamlit.text("Hello from Snowflake:")
+#streamlit.text(my_data_row)
